41357119cc9606c7fe581d035393a67d89280339
YanaLysukha <yanina.lysukha@gmail.com>
2024-02-28 15:44:42 +0100

    feat: implement isDateInPeriod function

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -134,8 +134,14 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const newDate = new Date(date);
+  const startDate = new Date(period.start);
+  const endDate = new Date(period.end);
+  if (newDate <= endDate && newDate >= startDate) {
+    return true;
+  }
+  return false;
 }
 
 /**
